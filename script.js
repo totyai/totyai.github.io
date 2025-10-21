f7fb991cee0105d0dc48b87a0d508ee83cd3798b
totyai <totyai18@gmail.com>
2025-10-21 22:19:12 +0200

    - Started adding DOM elements

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function rock_paper() {
+
+    draw = document.createElement("div");
+    document.createTextNode("")
+    document.getElementById("rock-paper").appendChild(draw);
     rock_div = document.getElementById("rock-paper");
-    console.log(rock_paper);
+    console.log(rock_div);
+    console.log(draw);
 }
